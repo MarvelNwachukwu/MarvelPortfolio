c4513934fe1e67b9c70ba82bac3c3f4da6b9b12e
Marvellous Nwachukwu <marvellousn561@gmail.com>
2020-12-07 16:24:28 +0100

    added intersection observers

--- main.js
+++ main.js
@@ -4,3 +4,36 @@ const nodeselector = (node) => {
 
 const techstackIndicator = nodeselector(".myTechStack");
 const techstack = nodeselector(".techStack");
+const portfolioIndicator = nodeselector("#myPortfolio");
+const portfolio = nodeselector(".portfolioText");
+
+let observer = new IntersectionObserver((e) => {
+  e.forEach((i) => {
+    if (i.isIntersecting === true) {
+      techstackIndicator.classList.remove("animatedButton");
+    } else {
+      techstackIndicator.classList.add("animatedButton");
+    }
+  });
+});
+
+let secondObserver = new IntersectionObserver((e) => {
+  e.forEach((i) => {
+    if (i.isIntersecting === true) {
+      portfolioIndicator.classList.remove("animatedButton");
+    } else {
+      portfolioIndicator.classList.add("animatedButton");
+    }
+  });
+});
+
+observer.observe(techstack);
+secondObserver.observe(portfolio);
+
+// JQuery
+// $(document).ready(function () {
+//   $(".myTechStack").hover(function () {
+//     console.log(true);
+//     $(".myTechStack").stop();
+//   });
+// });
